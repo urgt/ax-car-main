451ec47e669dbb5330c945cd8f4dcf6e8c8e27ae
G'ayrat Urinbaev <g.urinbaev@sxope.com>
2024-01-26 17:38:05 +0500

    AXMVP-51

--- views/card_componet.php
+++ views/card_componet.php
@@ -2,7 +2,7 @@
 
     <a href="<?php the_permalink(); ?>" class="main_page_catalog_cards_card" style="background-image: url('<?php
       $thumbnail_url = get_the_post_thumbnail_url(get_the_ID(), 'full');
-      $thumbnail_url_https = is_ssl() ? str_replace('http://', 'https://', $thumbnail_url) : $thumbnail_url;
+      $thumbnail_url_https = str_replace('http://', 'https://', $thumbnail_url);
       echo $thumbnail_url_https;
       ?>')">
 
