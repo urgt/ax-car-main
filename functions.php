52a7f7d266041282581c9f36a53d4ee5b93a106a
urinb <urinbaevg@gmail.com>
2024-04-04 16:38:10 +0500

    исправил: sitemap

--- functions.php
+++ functions.php
@@ -743,3 +743,33 @@ function motors_robots_txt(){
 
     die; // обрываем работу PHP
 }
+
+add_action( 'wp', function(){
+
+    if( is_sitemap_page() ){
+        function learn2smart_fix_wp_whitespace_issue($input) {
+
+            $allowed = false;
+            $found = false;
+
+            foreach (headers_list() as $header) {
+
+                if (preg_match("/^content-type:\s+(text\/(html|application\/(xhtml\+xml|atom\+xml|xml)))/i", $header)) {
+                    $allowed = true;
+                }
+
+                if (preg_match("/^content-type:\s+/i", $header)) {
+                    $found = true;
+                }
+            }
+
+            if ($allowed || $found) {
+                return preg_replace("/^\s*/m", "", $input);
+            } else {
+                return $input;
+            }
+        }
+
+        ob_start("learn2smart_fix_wp_whitespace_issue");
+    }
+} );
