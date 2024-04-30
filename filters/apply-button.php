aa8e7b44c7521fa323b0807deab2238c3432d0a9
urinb <urinbaevg@gmail.com>
2024-04-30 14:39:12 +0500

    sd

--- filters/apply-button.php
+++ filters/apply-button.php
@@ -11,17 +11,15 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-
-
 $set_id = isset($set['ID']) ? esc_html($set['ID']) : 0;
 ?>
 <div class="wpc-filters-section wpc-filters-section-<?php echo $set_id; ?> wpc-filter-layout-submit-button">
 <?php var_dump(remove_duplicates(esc_url($apply_url))); ?>
-    <a class="wpc-filters-submit-button" href="<?php echo remove_duplicates(esc_url($apply_url)); ?>"><?php
+    <a class="wpc-filters-submit-button" href="<?php echo esc_url($apply_url); ?>"><?php
          $button_text = isset($set['apply_button_text']['value']) ? esc_html($set['apply_button_text']['value']) : esc_html__('Apply', 'filter-everything');
          echo $button_text;
          ?></a>
-    <a class="wpc-filters-reset-button" href="<?php echo remove_duplicates(esc_attr($reset_url)) ?>"><?php
+    <a class="wpc-filters-reset-button" href="<?php echo esc_attr($reset_url) ?>"><?php
          $reset_button_text = isset($set['reset_button_text']['value']) ? esc_html($set['reset_button_text']['value']) : esc_html__('Reset', 'filter-everything');
          echo $reset_button_text;
          ?></a>
