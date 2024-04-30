316c8717878c0a26bcc3a45778114971729e1d20
urinb <urinbaevg@gmail.com>
2024-04-30 10:25:41 +0500

    style issues

--- views/filter.php
+++ views/filter.php
@@ -11,13 +11,13 @@ $current_language = pll_current_language();
     if ($env == 'prod') {
         if ($current_language == 'en') {
             ?>
-            <div style="order: 1">
+            <div style="order: 1" class="first">
                 <?php echo do_shortcode('[fe_widget id="4250"]') ?>
             </div>
-            <div style="order: 2">
+            <div style="order: 2" class="second">
                 <?php echo do_shortcode('[fe_widget id="4254"]') ?>
             </div>
-            <div style="order: 3">
+            <div style="order: 1" class="third"class="third">
                 <?php echo do_shortcode('[fe_widget id="4259"]') ?>
             </div>
             <?php
@@ -30,25 +30,25 @@ $current_language = pll_current_language();
     if ($env == 'staging') {
         if ($current_language == 'en') {
             ?>
-            <div style="order: 1">
+            <div style="order: 1" class="first">
                 <?php echo do_shortcode('[fe_widget id="3447"]') ?>
             </div>
-            <div style="order: 2">
+            <div style="order: 2" class="second">
                 <?php echo do_shortcode('[fe_widget id="3451"]') ?>
             </div>
-            <div style="order: 3">
+            <div style="order: 3" class="third">
                 <?php echo do_shortcode('[fe_widget id="3412"]') ?>
             </div>
             <?php
         } else if ($current_language == 'ru') {
             ?>
-             <div style="order: 1">
+             <div style="order: 1" class="first">
                 <?php echo do_shortcode('[fe_widget id="3497"]') ?>
             </div>
-            <div style="order: 2">
+            <div style="order: 2" class="second">
                 <?php echo do_shortcode('[fe_widget id="3501"]') ?>
             </div>
-            <div style="order: 3">
+            <div style="order: 3" class="third">
                 <?php echo do_shortcode('[fe_widget id="3510"]') ?>
             </div>
             <?php
@@ -58,25 +58,25 @@ $current_language = pll_current_language();
     if ($env == 'local') {
         if ($current_language == 'en') {
             ?>
-            <div style="order: 1">
+            <div style="order: 1" class="first">
                 <?php echo do_shortcode('[fe_widget id="3447"]') ?>
             </div>
-            <div style="order: 2">
+            <div style="order: 2" class="second">
                 <?php echo do_shortcode('[fe_widget id="3451"]') ?>
             </div>
-            <div style="order: 3">
+            <div style="order: 3" class="third">
                 <?php echo do_shortcode('[fe_widget id="3412"]') ?>
             </div>
             <?php
         } else if ($current_language == 'ru') {
             ?>
-             <div style="order: 1">
+             <div style="order: 1" class="first">
                 <?php echo do_shortcode('[fe_widget id="3497"]') ?>
             </div>
             <div style="order: 2">
                 <?php echo do_shortcode('[fe_widget id="3501"]') ?>
             </div>
-            <div style="order: 3">
+            <div style="order: 3" class="third">
                 <?php echo do_shortcode('[fe_widget id="3510"]') ?>
             </div>
             <?php
